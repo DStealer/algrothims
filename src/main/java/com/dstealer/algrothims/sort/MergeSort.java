990468db5a01f8db8584f365d689a8e0928e02ac
LiShiwu <lishiwu@zhonggu.cn>
2018-08-08 15:55:34 +0800

    1归并排序

--- src/main/java/com/dstealer/algrothims/sort/MergeSort.java
+++ src/main/java/com/dstealer/algrothims/sort/MergeSort.java
@@ -6,26 +6,56 @@ package com.dstealer.algrothims.sort;
  * 1 拆分各个小数组
  * 2 合并各个小的有序数组
  * o(nlog2n)
+ * 优化思路:
+ * 1 小数组使用插入排序
+ * 2 已经有序的不需要排序
  */
 public class MergeSort {
-    public static void sort(int[] arr) {
-        _sort(arr, 0, arr.length - 1);
+    public static void sort(int[] arr, int flag) {
+        switch (flag) {
+            case 1:
+                _sortTop2Bottom(arr, 0, arr.length - 1);
+                break;
+            case -1:
+                _sortBottom2Top(arr, arr.length);
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 
     /**
+     * 使用递归从top到bottom归并
+     *
      * @param arr
      * @param l
      * @param r
      */
-    private static void _sort(int[] arr, int l, int r) {
+    private static void _sortTop2Bottom(int[] arr, int l, int r) {
         int mid = (l + r) / 2; //越界问题
         if (l < mid) {
-            _sort(arr, l, mid);
+            _sortTop2Bottom(arr, l, mid);
         }
         if (mid + 1 < r) {
-            _sort(arr, mid + 1, r);
+            _sortTop2Bottom(arr, mid + 1, r);
         }
-        merge(arr, l, mid, r);
+        if (arr[mid] < arr[mid + 1]) { //有序优化,已经有序
+            merge(arr, l, mid, r);
+        }
+    }
+
+    /**
+     * 循环处理,从bottom到top归并
+     *
+     * @param arr
+     * @param n
+     */
+    private static void _sortBottom2Top(int[] arr, int n) {
+        for (int sz = 1; sz <= n; sz += sz) {
+            for (int i = 0; i < n; i += sz + sz) {
+                merge(arr, i, i + sz - 1, Math.min(i + sz + sz - 1, n - 1));
+            }
+        }
     }
 
     /**
--- src/test/java/com/dstealer/algrothims/sort/MergeSortTest.java
+++ src/test/java/com/dstealer/algrothims/sort/MergeSortTest.java
@@ -9,13 +9,17 @@ public class MergeSortTest {
     public void sort() {
         int[] arr = Assistant.generateRandomArray(10, 100);
         Assistant.print(arr);
-        MergeSort.sort(arr);
+        MergeSort.sort(arr, 1);
         Assistant.print(arr);
         Assistant.checkAsc(arr);
     }
 
     @Test
     public void sort1() {
-        MergeSort.merge(new int[]{53, 69, 78, 30, 76}, 0, 2, 4);
+        int[] arr = Assistant.generateRandomArray(10, 100);
+        Assistant.print(arr);
+        MergeSort.sort(arr, -1);
+        Assistant.print(arr);
+        Assistant.checkAsc(arr);
     }
 }
